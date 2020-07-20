6cb4676a49510a780ec29a89a46b43f2aa703a0e
shengliangge <850110183@qq.com>
2020-07-20 14:38:43 +0800

    更新

---
+++ css/rem/readme.md
@@ -0,0 +1,3 @@
+相对单位
+rem：相对于  html  font-size 
+em:  相对于 自己的 font-size
---
+++ es6/变量提升/let_const.js
@@ -0,0 +1,27 @@
+// var a = [];
+// for (var i = 0; i < 10; i++) {
+//     a[i] = function() {
+//         cosole.log(i);
+
+//     };
+//     a[i]();
+// }
+// // a[6]();
+
+// for (let i = 0; i < 3; i++) { //父级作用域
+//     i = 'abc'; //子作用域
+//     console.log(i);
+// }
+
+
+
+// 暂时性死区
+// var tmp = 123;
+// if (true) {
+//     tem = 'abc';
+//     let tmp;
+// }
+
+let a = 'a'
+let a = 'aa'
+console.log(a)
