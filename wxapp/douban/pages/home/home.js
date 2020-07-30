910e08683b50c3a9b24b03b5247e3844304987ee
shengliangge <850110183@qq.com>
2020-07-30 10:10:17 +0800

    提交

--- wxapp/douban/pages/home/home.js
+++ wxapp/douban/pages/home/home.js
@@ -6,17 +6,75 @@ Page({
    */
   data: {
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    movie:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCity((city) => {
+      this.loadData(0,{city:city,apikey:'0df993c66c0c636e29ecbb5344252a4a'})
+    })
     this.setData({
       search: this.search.bind(this)
     })
   },
+  loadData(idx,params) {
+    let url=wx.db.url('/v2/movie/in_theaters')
+    wx.request({
+      url: url,
+      data: params,
+      header:{'content-type':'json'},
+      success:  (res)=> {
+        console.log(res)
+        this.setData({
+          
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  getCity(succeed) {
+    //拿到当前所在的城市名称
+    //先拿到当前所在区域的经纬度（小程序自带api）
+    wx.getLocation({
+      success: function (res) {
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'rptXHa8hm7ohCXbNNr9qNX83qKASOGf9',
+            output: 'json',
+            coordtype: 'wgs84ll',
+            location: `${res.latitude},${res.longitude}`
+          },
+          success: (res) => {
+            // console.log(res)
+            let city = res.data.result.addressComponent.city
+            succeed && succeed(city)
+          },
+          fail: () => {
+            console.log('获取当前城市失败')
+          }
+        })
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
+
   search: function (value) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
