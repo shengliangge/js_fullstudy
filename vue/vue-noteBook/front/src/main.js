5a9a8dc333ad29d076ec2c56ad93b959663e2022
shengliangge <850110183@qq.com>
2020-09-16 09:30:09 +0800

    提交

--- vue/vue-noteBook/front/src/main.js
+++ vue/vue-noteBook/front/src/main.js
@@ -2,7 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+import { Swipe, SwipeItem, Icon, Field, ActionSheet, Toast, Panel, Uploader } from 'vant'
+import 'vant/lib/index.css'
+import util from './assets/js/util'
+import axios from 'axios'
 
+Vue.use(Swipe).use(SwipeItem).use(Icon).use(Field).use(ActionSheet).use(Toast).use(Panel).use(Uploader).use(axios)
+
+Vue.prototype.$http = axios
+Vue.prototype.$util = util
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
 Vue.config.productionTip = false
 
 new Vue({
--- vue/vue-noteBook/note-book/routes/users.js
+++ vue/vue-noteBook/note-book/routes/users.js
@@ -2,16 +2,72 @@ const router = require('koa-router')()
 const userService = require('../controllers/mySqlConfig')
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
 router.get('/all', async function (ctx, next) {
   await userService.getAllUsers().then((res) => {
     ctx.body = res
+  })
+})
+// 登录注册
+router.post('/userRegister', async (ctx, next) => {
+  var _username = ctx.request.body.username
+  var _userpwd = ctx.request.body.userpwd
+  var _nickname = ctx.request.body.nickname
+  if (!_username || !_userpwd || !_nickname) {
+    ctx.body = {
+      code: '80001',
+      mess: '用户名或密码或昵称不能为空'
+    }
+    return
+  }
+  let user = {
+    username: _username,
+    userpwd: _userpwd,
+    nickname: _nickname
+  }
+  await userService.findUser(user.username).then(async (res) => {
+    console.log(res);
+    if (res.length) {
+      try {
+        throw Error('用户名已存在')
+      } catch (error) {
+        console.log(error);
+      }
+      ctx.body = {
+        code: '80003',
+        data: 'err',
+        mess: '用户名已存在'
+      }
+    } else {
+      await userService.insertUser([user.username, user.userpwd, user.nickname]).then(async (res) => {
+        let r = ''
+        if (res.affectedRows !== 0) {
+          r = 'ok'
+          ctx.body = {
+            code: '80000',
+            data: r,
+            mess: '注册成功'
+          }
+        } else {
+          r = 'error'
+          ctx.body = {
+            code: '80004',
+            data: r,
+            mess: '注册失败'
+          }
+        }
+      }).catch((err) => {
+        ctx.body = {
+          code: '80002',
+          mess: err
+        }
+      })
+    }
+  }).catch((err) => {
+    ctx.body = {
+      cody: '80002',
+      mess: err
+    }
   })
 })
 module.exports = router
