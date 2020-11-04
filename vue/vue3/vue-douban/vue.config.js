80c5a945195db63809d2cf2471c30e32f7528294
shengliangge <850110183@qq.com>
2020-11-04 10:08:00 +0800

    提交

--- vue/vue3/vue-douban/vue.config.js
+++ vue/vue3/vue-douban/vue.config.js
@@ -7,7 +7,12 @@ function resolve(dir) {
 module.exports = {
   devServer: {
     proxy: {
-      '/api': ''
+      '/api': {
+        target: 'http://t.yushu.im',
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
     }
   },
   configureWebpack: {
@@ -16,19 +21,19 @@ module.exports = {
         '@': resolve('src')
       }
     }
-  }
-}
-css: {
-  loaderOptions: {
-    less: {
-      // 若使用 less-loader@5，请移除 lessOptions 这一级，直接配置选项。
-      modifyVars: {
-        'nav-bar-background-color': '#42bd56',
+  },
+
+  css: {
+    loaderOptions: {
+      less: {
+        // 若使用 less-loader@5，请移除 lessOptions 这一级，直接配置选项。
+        modifyVars: {
+          'nav-bar-background-color': '#42bd56',
           'nav-bar-icon-color': '#fff',
-            'nav-bar-text-color': '#fff',
-              'nav-bar-title-text-color': '#fff'
+          'nav-bar-text-color': '#fff',
+          'nav-bar-title-text-color': '#fff'
+        },
       },
     },
   },
-},
 };
